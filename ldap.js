2734e209f238a4d7d108de2d38a0db812a1b04ab
Alan Souza <alansouzati@gmail.com>
2016-06-08 17:15:49 -0700

    Fixed case sensitive search

--- ldap.js
+++ ldap.js
@@ -1,8 +1,45 @@
 import ldap from 'ldapjs';
+import helpers from 'ldap-filter/lib/helpers';
 
 const server = ldap.createServer();
 
-const PORT = process.env.PORT || 389;
+const PORT = process.env.PORT || 1389;
+
+function escapeRegExp(str) {
+  /* JSSTYLED */
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+}
+
+ldap.SubstringFilter.prototype.matches = function (target, strictAttrCase) {
+  var tv = helpers.getAttrValue(target, this.attribute, strictAttrCase);
+  if (tv !== undefined && tv !== null) {
+    var re = '';
+
+    if (this.initial)
+      re += '^' + escapeRegExp(this.initial) + '.*';
+    this.any.forEach(function (s) {
+      re += escapeRegExp(s) + '.*';
+    });
+    if (this.final)
+      re += escapeRegExp(this.final) + '$';
+
+    var matcher = new RegExp(re, 'i');
+    return helpers.testValues(function (v) {
+      return matcher.test(v);
+    }, tv);
+  }
+
+  return false;
+};
+
+ldap.EqualityFilter.prototype.matches = function (target, strictAttrCase) {
+  var tv = helpers.getAttrValue(target, this.attribute, strictAttrCase);
+  var value = this.value.toLowerCase();
+
+  return helpers.testValues(function (v) {
+    return value === v.toLowerCase();
+  }, tv);
+};
 
 function createLdapUser (username, name, avatar) {
   return {
