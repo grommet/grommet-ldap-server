fb511d466093ec958d00fb11ce8ceef7a571b635
Alan Souza <alansouzati@gmail.com>
2016-06-20 10:53:50 -0700

    Added one more person and location. Fixed timezone for ft.collins.

--- locations.js
+++ locations.js
@@ -10,13 +10,12 @@ export default {
       "postalCode": "94304-1112",
       "co": "United States",
       "street": "3000 Hanover St.",
-      "timeZoneName": "Pacific Time (US & Canada); Tijuana",
       "st": "California",
       "latitude": "+37.3939",
       "longitude": "-122.1700",
       "l": "Palo Alto",
       "c": "US",
-      "timeZone": "-0800",
+      "timeZone": "-0700",
       "cn": "PAL20",
       "telephoneNumber": "+1 (555) 555-5555",
       "category": "Headquarters"
@@ -33,16 +32,37 @@ export default {
       "co": "United States",
       "street": "3404 E Harmony Rd.",
       "latitude": "+40.5233",
-      "timeZoneName": "Mountain Time (US & Canada)",
       "longitude": "-105.0370",
       "postalCode": "80528-9544",
       "postalAddress": "3404 E Harmony Rd. $ Ft. Collins $ Colorado $ United States $ 80528-9544",
       "l": "Ft. Collins",
       "c": "US",
-      "timeZone": "-0700",
+      "timeZone": "-0600",
       "cn": "FTC06",
       "telephoneNumber": "+1 (555) 555-5555",
       "category": "Main Branch"
+    }
+  },
+  "LUNDE01": {
+    "dn": "lid=LUNDE01, ou=locations, o=grommet.io",
+    attributes: {
+      "postalAddress": "Nöbbelövs torg 2 $ Lund $ Scania $ Sweden $ 226 52",
+      "lid": "LUNDE01",
+      "objectClass": [
+        "locality"
+      ],
+      "postalCode": "226 52",
+      "co": "Sweden",
+      "street": "Nöbbelövs torg 2",
+      "st": "Scania",
+      "latitude": "+55.728502",
+      "longitude": "+13.174933",
+      "l": "Lund",
+      "c": "SE",
+      "timeZone": "+0200",
+      "cn": "LUNDE01",
+      "telephoneNumber": "+1 (555) 555-5555",
+      "category": "Sweden Branch"
     }
   }
 };
